3f6c72c904ce7a38d84e850b051179b4a818c874
Yolanda Alejo Huerta <za240113432@zapopan.tecmm.edu.mx>
2024-10-04 21:40:44 -0600

    preguntando mas

--- tarea1/asistencia.py
+++ tarea1/asistencia.py
@@ -11,8 +11,9 @@ class Ponderacion:
     Es usada para la saber lo que se va a calificar
     """
 
-    def __init__(self, nombre: str, numero_maximo: int):
+    def __init__(self, nombre: str, porcentaje: int, numero_maximo: int = 0):
         self.nombre = nombre
+        self.porcentaje = porcentaje
         self.maximo = numero_maximo
 
 
@@ -38,9 +39,26 @@ def principal():
 
     print("Programa que te ayuda a calcular el promedio de un alumno\n\n")
 
+    # Pregunta por los elementos que vamos a evaluar
     for elemento in ["Tareas", "Examenes", "Participación", "Asistencia"]:
-        numero = int(input(f"¿Cuanto es el porcentaje de las {elemento}?"))
-        cal.add(Ponderacion(elemento, numero))
+        porcentaje = int(input(f"¿Cuánto es el porcentaje de las {elemento}?: "))
+        num_max = 0
+
+        if elemento != "Examenes":
+            num_max = int(input(f"¿Cuántas {elemento} fueron?: "))
+
+        cal.add(Ponderacion(elemento, porcentaje, num_max))
+
+    # Variable para saber si queremos salir
+    salir = False
+
+    while not salir:
+        print("\nIngresa las calificaciones de un alumno")
+        print("   Si son Tareas pon cuantas tareas entrego.")
+        print("   Si son Examenes pon su calificación")
+        print("   Si es participación cuantas participaciones tubo")
+        print("   Si son Asitencias pon cuantas asistencias tubo.")
+        salir = True
 
 
 if __name__ == "__main__":
