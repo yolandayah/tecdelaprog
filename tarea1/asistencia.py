eb8efec1796fe78a74223a79e03aeca0f176450a
Yolanda Alejo Huerta <za240113432@zapopan.tecmm.edu.mx>
2024-10-04 20:42:13 -0600

    iniciando asistencia

---
+++ tarea1/asistencia.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# vi: set shiftwidth=4 tabstop=8 expandtab:
+"""
+Ejemplo de una mónada para calcular el promedio de alumnos
+"""
+
+def main():
+    """
+    Comentario de la función
+    """
+    print("Hola Mundo")
+
+
+if __name__ == "__main__":
+    main()
+
